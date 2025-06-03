1b4b64ebb07669a89911ee23f4e7cb0dcbf2a933
Ryan Hui <ryanhui30@gmail.com>
2025-06-03 16:49:43 -0700

    Add stats card component

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,7 +1,10 @@
-import { Header } from 'components'
+import { Header, StatsCard, TripCard } from 'components'
+import { dashbaordStats, users, allTrips } from '~/constants';
 
 const Dashboard = () => {
   const user = { name: 'Ryan'};
+
+  const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } = dashbaordStats;
 
   return (
     <main className="dashboard wrapper">
@@ -10,7 +13,34 @@ const Dashboard = () => {
           description="Track activity, trends and popular destinations in real time"
         />
 
-        Dashboard Page Contents
+        <section className="flex flex-col gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+
+            <StatsCard
+              headerTitle="Total Users"
+              total={totalUsers}
+              currentMonthCount={usersJoined.currentMonth}
+              lastMonthCount={usersJoined.lastMonth}
+            />
+
+            <StatsCard
+              headerTitle="Total Trips"
+              total={totalUsers}
+              currentMonthCount={tripsCreated.currentMonth}
+              lastMonthCount={tripsCreated.lastMonth}
+            />
+
+            <StatsCard
+              headerTitle="Active Users"
+              total={userRole.total}
+              currentMonthCount={userRole.currentMonth}
+              lastMonthCount={userRole.lastMonth}
+            />
+          </div>
+
+        </section>
+
+        <TripCard />
     </main>
   )
 }
